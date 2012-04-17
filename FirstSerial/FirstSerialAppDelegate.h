6c341760dfdbfa653c238752c20f5b9c2781911f
Gene Pope <gene@blackfoot.net>
2012-04-16 19:07:26 -0700

    from Xcode

---
+++ FirstSerial/FirstSerialAppDelegate.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+@class FirstSerialViewController;
+
+@interface FirstSerialAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FirstSerialViewController *viewController;
+
+@end
